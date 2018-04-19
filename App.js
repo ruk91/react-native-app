444643557cc3d8cf601b35fcd8ab29877fe4e2b2
sajith <sajith@tsdclanka.com>
2018-04-19 09:32:09 +0530

    drawer edited

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import { DrawerNavigator} from 'react-navigation';
 import Icon from "react-native-vector-icons/FontAwesome";
 import tabNav from './src/navigation/tabnav'
+import { View, Text, StyleSheet, Button, TouchableOpacity, ImageBackground } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components';
+
+let height = Navigator.NavigationBar.Styles.General.TotalNavHeight
 
 const drawernav = DrawerNavigator({
     DrawerItem1: {
         screen: tabNav,
         navigationOptions: {
-            drawerLabel: "Drawer Item 1",
-            drawerIcon: ({ tintColor }) => <Icon name="rocket" size={24} />
+            //drawerLabel: "Drawer Item 1",
+            drawerIcon: ({ tintColor }) => 
+            <View style={{alignItems:'center', justifyContent:'center', marginTop: height}}>
+                <Text>test</Text>
+            </View>
         },
     },
     DrawerItem2: {
@@ -17,7 +24,15 @@ const drawernav = DrawerNavigator({
             drawerLabel: "Scan Me",
             drawerIcon: ({ tintColor }) => <Icon name="rocket" size={24} />
         },
-    }
+    },
+    DrawerItem3: {
+        screen: tabNav,
+        navigationOptions: {
+            drawerLabel: "Drawer Item 3",
+            drawerIcon: ({ tintColor }) => 
+            <Icon name="rocket" size={24} />
+        },
+    },
 });
 
 export default drawernav;
