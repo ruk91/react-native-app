1d35b28270d2c4524553a10ff614658c4590365e
Inod Wagachchi <inod@tsdclanka.com>
2018-04-25 23:22:21 +0530

    Initial Commit

---
+++ src/Data/flatListData.js
@@ -0,0 +1,64 @@
+let flatListData = [
+    {
+        'key': 'F001',
+        //'name': 'Milk Tea',
+        //'imageURL': 'https://hutch-q1448gllrgm6mjfqg.netdna-ssl.com/wp-content/uploads/2016/03/Banner.jpg',
+        //'foodDescription': 'Rs: 30.00'
+    },
+    {
+        'key': 'F002',
+        'name': 'Plain Tea',
+        'imageURL': 'http://static.asiawebdirect.com/m/kl/portals/srilanka-hotels-ws/homepage/hotels/pagePropertiesImage/sri-lanka-hotels.jpg',
+        'foodDescription': 'Rs: 20.00'
+    },
+    {
+        'key': 'F003',
+        'name': 'Egg Bun',
+        'imageURL': 'https://www.aitkenspencehotels.com/wp-content/uploads/2014/08/EarlsRegency.jpg',
+        'foodDescription': 'Rs: 60.00'
+    },
+    {
+        'key': 'F004',
+        'name': 'Fish Bun',
+        'imageURL': 'https://t-ec.bstatic.com/images/hotel/max1024x768/948/94835315.jpg',
+        'foodDescription': 'Rs: 40.00'
+    },
+    {
+        'key': 'F005',
+        'name': 'Bread',
+        'imageURL': 'https://www.aitkenspencehotels.com/wp-content/uploads/2014/08/BandarawelaHotel.jpg',
+        'foodDescription': 'Rs: 60.00'
+    },
+    {
+        'key': 'F006',
+        //'name': 'Doughnuts',
+        //'imageURL': 'https://hutch-q1448gllrgm6mjfqg.netdna-ssl.com/wp-content/uploads/2015/09/07-Trendy-Tips.jpg',
+        //'foodDescription': 'Rs: 55.00'
+    },
+    {
+        'key': 'F007',
+        'name': 'Pastry',
+        'imageURL': 'http://www.resort98acres.com/wp-content/uploads/2013/04/02-Home.jpg',
+        'foodDescription': 'Rs: 40.00'
+    },
+    {
+        'key': 'F008',
+        'name': 'Butter Cake',
+        'imageURL': 'https://d3dz4rogqkqh6r.cloudfront.net/uploads/files/2016/08/yimg_AiYgpJ.jpg',
+        'foodDescription': 'Rs: 350.00'
+    },
+    {
+        'key': 'F009',
+        'name': 'Chocolate Cake',
+        'imageURL': 'https://www.aitkenspencehotels.com/wp-content/uploads/2014/08/HeritanceKandalama.jpg',
+        'foodDescription': 'Rs: 425.00'
+    },
+    {
+        'key': 'F010',
+        'name': 'Rolls',
+        'imageURL': 'http://bestoflanka.com/images/recommended-stays-sri-lanka/stay-in-the-cultural-triangle-sri-lanka/02.jpg',
+        'foodDescription': 'Rs: 35.00'
+    }    
+]
+
+export default flatListData;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,41 +1,108 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Dimensions, ScrollView, TouchableOpacity} from 'react-native';
-import GridView from 'react-native-super-grid';
+import { View, Text, StyleSheet, Dimensions, ScrollView, TouchableOpacity, FlatList, Image} from 'react-native';
+import { responsiveHeight, responsiveWidth, responsiveFontSize } from 'react-native-responsive-dimensions';
+
+import flatListData from '../Data/flatListData'
+import Swipeout from 'react-native-swipeout'
 
 const window = Dimensions.get('window');
 
-class Home extends Component {
+class FlatListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activeRowKey: null
+        }
+    }
+  
     render() {
-        const items = [
-            { name: '98 Acres, Ella', code: '#1abc9c' }, { name: 'EMERALD', code: '#2ecc71' },
-            { name: 'PETER RIVER', code: '#3498db' }, { name: 'AMETHYST', code: '#9b59b6' },
-            { name: 'WET ASPHALT', code: '#34495e' }, { name: 'GREEN SEA', code: '#16a085' },
-            { name: 'NEPHRITIS', code: '#27ae60' }, { name: 'BELIZE HOLE', code: '#2980b9' },
-            { name: 'WISTERIA', code: '#8e44ad' }, { name: 'MIDNIGHT BLUE', code: '#2c3e50' },
-            { name: 'SUN FLOWER', code: '#f1c40f' }, { name: 'CARROT', code: '#e67e22' },
-            { name: 'ALIZARIN', code: '#e74c3c' }, { name: 'CLOUDS', code: '#ecf0f1' },
-            { name: 'CONCRETE', code: '#95a5a6' }, { name: 'ORANGE', code: '#f39c12' },
-            { name: 'PUMPKIN', code: '#d35400' }, { name: 'POMEGRANATE', code: '#c0392b' },
-            { name: 'SILVER', code: '#bdc3c7' }, { name: 'ASBESTOS', code: '#7f8c8d' },
-          ];
-
+        const swipeSettings = {
+            autoClose: true,
+            onClose: (secId, rowId, direction) => {
+                if(this.state.activeRowKey !== null) {
+                    this.setState({ activeRowKey: null})
+                }
+            },
+            onOpen: (secId, rowId, direction) => {
+                this.setState({ activeRowKey: this.props.item.key })
+            },
+            right: [
+                {
+                    onPress: () => {
+                        const deletingRow = this.state.activeRowKey;
+                        Alert.alert(
+                            'Alert',
+                            'Do you really want to activate this package ?',
+                            [
+                                {text: 'No', onPress: () => console.log('cancel Pressed'), style: 'cancel'},
+                                {text: 'Yes', onPress: () => {
+                                    flatListData.splice(this.props.index, 1);
+                                    // Refresh FlatList
+                                    this.props.parentFlatList.refreshFlatList(deletingRow)
+                                }}
+                            ],
+                            { cancelable: true}
+                        )
+                    },
+                    text: 'Activate', type: 'delete'
+                }
+            ],
+            rowId: this.props.index,
+            sectionId: 1
+        };
+        return (
+            <Swipeout {...swipeSettings}>
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    {/* <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#154360' }}> */}
+                
+                        <Image source={{uri: this.props.item.imageURL}} resizeMode='stretch' style={{width: responsiveWidth(88), height: 150, margin: 5}} />
+    
+                        {/* <View style={{ flex: 1, flexDirection:'column'}}>
+                            <Text style={styles.FlatListItem}> {this.props.item.name} </Text>
+                            <Text style={styles.FlatListItem}> {this.props.item.foodDescription} </Text>
+                        </View> */}
+  
+                    </View>
+                    <View style={{flex:1, height: 3, backgroundColor: 'white'}} />
+                {/* </View> */}
+            </Swipeout>
+        )
+    }
+  }
+  
+class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          deleteRowKey: null
+        };
+      }
+  
+      // refresh by changing state
+      refreshFlatList = (deleteKey) => {
+        this.setState(() => {
+          return {
+            deleteRowKey: deleteKey
+          }
+        })
+      };
+  
+    render() {
         return (
             <View style={styles.container}>
                 <ScrollView>
                     <View style={styles.container1}>
-                        <GridView
-                            itemDimension={130}
-                            items={items}
-                            style={styles.gridView}
-                            renderItem={item => (
-                                <TouchableOpacity>
-                                    <View style={[styles.itemContainer, { backgroundColor: item.code }]}>
-                                        <Text style={styles.itemName}>{item.name}</Text>
-                                        <Text style={styles.itemCode}>{item.code}</Text>
-                                    </View>
-                                </TouchableOpacity>
-                            )}
-                        />
+                        <FlatList
+                            data={flatListData}
+                            renderItem={({item, index})=>{
+                            //console.log('Item = ${JSON.stringify(item)}, index = ${index}')
+                            return (
+                                <FlatListItem item={item} index={index} parentFlatList={this}>
+
+                                </FlatListItem>
+                            )
+                            }}>
+                        </FlatList>
                     </View>
                 </ScrollView>
                 
@@ -58,27 +125,7 @@ const styles = StyleSheet.create({
         backgroundColor:'rgba(255, 255, 255, 0.3)',
         justifyContent: 'center',
         alignItems: 'center',
-    },
-    gridView: {
-        paddingTop: 25,
-        flex: 1,
-      },
-      itemContainer: {
-        justifyContent: 'flex-end',
-        borderRadius: 5,
-        padding: 10,
-        height: 150,
-      },
-      itemName: {
-        fontSize: 16,
-        color: '#fff',
-        fontWeight: '600',
-      },
-      itemCode: {
-        fontWeight: '600',
-        fontSize: 12,
-        color: '#fff',
-      },
+    }
 });
 
 export default Home;
